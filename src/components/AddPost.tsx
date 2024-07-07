2185ab2b03abe1ce40bc8b47d3df96a23cced12d
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-07 19:19:43 +0330

    Until consol log of userId

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,27 +1,39 @@
-"use client";
-
+import prisma from "@/lib/client";
 import { useUser } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 // import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
-import { useState } from "react";
 // import AddPostButton from "./AddPostButton";
 // import { addPost } from "@/lib/actions";
 
 const AddPost = () => {
-  const { user, isLoaded } = useUser();
-  const [desc, setDesc] = useState("");
-  const [img, setImg] = useState<any>();
+  const { userId } = auth();
+  if (!userId) return;
+  console.log(userId);
 
-  if (!isLoaded) {
-    return "Loading...";
-  }
+  const testAction = async (formData: FormData) => {
+    "use server";
+    const desc = formData.get("desc") as string;
+
+    try {
+      const res = await prisma.post.create({
+        data: {
+          userId: userId,
+          desc: desc,
+        },
+      });
+
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* AVATAR */}
       <Image
-        src={ "/noAvatar.png"}
+        src={"/noAvatar.png"}
         alt=""
         width={48}
         height={48}
@@ -30,12 +42,11 @@ const AddPost = () => {
       {/* POST */}
       <div className="flex-1">
         {/* TEXT INPUT */}
-        <form  className="flex gap-4">
+        <form action={testAction} className="flex gap-4">
           <textarea
             placeholder="What is on your mind?"
             className="flex-1 bg-slate-100 rounded-lg p-2"
             name="desc"
-            onChange={(e) => setDesc(e.target.value)}
           ></textarea>
           <div className="">
             <Image
@@ -46,10 +57,10 @@ const AddPost = () => {
               className="w-5 h-5 cursor-pointer self-end"
             />
           </div>
+          <button>Click</button>
         </form>
         {/* POST OPTIONS */}
         <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
-        
           <div className="flex items-center gap-2 cursor-pointer">
             <Image src="/addVideo.png" alt="" width={20} height={20} />
             Video
